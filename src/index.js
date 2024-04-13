896852f4a8d9718c90c4e82e10277e7541e0c4c7
rishik puneet m <rishikpuneetm@gmail.com>
2024-04-13 17:26:54 +0530

    utils Code discussion

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-  path: "./.env"
+  path: "./.env",
 });
 
-
 connectDB()
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 4123, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.error("MongoDB connection error: ", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error: ", error);
+    throw error;
+  });
 
 // import express from "express";
 
 // const app = express();
 
-
-
-
 // const connectDB = async () => {
 //   try {
 //     mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}` )
-//     app.on("error", (error) => {
-//       console.error("MongoDB connection error: ",error);
-//       throw error;
-//     })
-
+// app.on("error", (error) => {
+//   console.error("MongoDB connection error: ",error);
+//   throw error;
+// })
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server listening on port ${process.env.PORT}`);
@@ -39,6 +42,3 @@ connectDB()
 //     throw error;
 //   }
 // }
-
-
-
